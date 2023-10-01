09f984dac9664b970d29d86dced27980c06470cc
hugoresende <hugo.resende@iservices.pt>
2023-10-01 21:05:13 +0100

    Vue.js Basics (part 1)

---
+++ README.md
@@ -0,0 +1,4 @@
+# https://www.youtube.com/watch?v=F7PLPJqVotk&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=2&ab_channel=NetNinja
+
+### v-on can be @
+### vue js 3 tutorial for beginners
--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log('hello vue')
+
+const app = Vue.createApp({
+    // data, functions
+    // template: '<h2>I am a dynamic template</h2>',
+    data(){
+        return{
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    },
+    methods: {
+        changeTitle(title) {
+            console.log('you clicked me')
+            // this.title = "Words of Randiance"
+            this.title = title
+        }
+    }
+})
+
+app.mount('#app')

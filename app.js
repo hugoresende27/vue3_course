d1d1d97e0fa5e3728c5833eaaddd2af2f242d6df
hugoresende <hugo.resende@iservices.pt>
2023-10-03 20:09:59 +0100

    lesson 2

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = Vue.createApp({
     // template: '<h2>I am a dynamic template</h2>',
     data(){
         return{
+            showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
             age: 45
@@ -15,6 +16,9 @@ const app = Vue.createApp({
             console.log('you clicked me')
             // this.title = "Words of Randiance"
             this.title = title
+        },
+        ToogleShowBooks(){
+            this.showBooks = !this.showBooks
         }
     }
 })

c084d4ba25e0e27a56d2228490d4a05c115805ea
hugoresende <hugo.resende@iservices.pt>
2023-10-03 21:50:53 +0100

    v-bind:

--- app.js
+++ app.js
@@ -8,17 +8,35 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0,
+            books: [
+                { title: 'name of the wind', author : 'Zecas'},
+                { title: 'Hello from the other side', author : 'Tonecas'},
+                { title: 'Quim porta é ter saúde', author : 'Maria'},
+            ],
+            url : 'http://www.thenetninja.co.uk'
         }
     },
     methods: {
-        changeTitle(title) {
+        changeTitle(title){
             console.log('you clicked me')
             // this.title = "Words of Randiance"
             this.title = title
         },
         ToogleShowBooks(){
             this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data){
+            console.log(e, e.type)
+            if(data){
+                console.log(data)
+            }
+        },
+        handleMouseMove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
